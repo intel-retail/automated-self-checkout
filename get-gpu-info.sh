757e73cf11dd4ff7fb54a0cecd3ab211f7414d09
brian-intel <brian.mcginn@intel.com>
2023-05-19 11:08:25 -0700

    fix: replace dmesg with the more stable lspci command (#35)

--- get-gpu-info.sh
+++ get-gpu-info.sh
@@ -14,13 +14,11 @@ GPU_NUM_170=0
 
 #get_gpu_devices() {
 	has_gpu=0
-	has_any_intel_non_server_gpu=`dmesg | grep -i "class 0x030000" | grep "8086"`
-	has_any_intel_server_gpu=`dmesg | grep -i "class 0x038000" | grep "8086"`
-	has_flex_170=`echo "$has_any_intel_server_gpu" | grep -i "56C0"`
-	has_flex_140=`echo "$has_any_intel_server_gpu" | grep -i "56C1"`
-	has_arc=`echo "$has_any_intel_non_server_gpu" | grep -iE "5690|5691|5692|56A0|56A1|56A2|5693|5694|5695|5698|56A5|56A6|56B0|56B1|5696|5697|56A3|56A4|56B2|56B3"`
+	has_flex_170=`lspci -d :56C0`
+	has_flex_140=`lspci -d :56C1`
+	has_arc=`lspci | grep -iE "5690|5691|5692|56A0|56A1|56A2|5693|5694|5695|5698|56A5|56A6|56B0|56B1|5696|5697|56A3|56A4|56B2|56B3"`
 
-	if [ -z "$has_any_intel_non_server_gpu" ] && [ -z "$has_any_intel_server_gpu" ] 
+	if [ -z "$has_flex_170" ] && [ -z "$has_flex_140" ] && [ -z "$has_arc" ] 
 	then
 		echo "No Intel GPUs found"
 		return
