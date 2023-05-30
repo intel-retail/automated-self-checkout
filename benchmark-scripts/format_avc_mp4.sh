3ed6607d02a8aa72f8de169c0acecbfc11e222a4
Valina Li <valina.li@intel.com>
2023-05-30 10:57:33 -0700

    fix: fixed download_sample_videos.sh should support non dgpu systems (#49)
    * fix: fixed download_sample_videos.sh should support non dgpu systems
    
    Closes: issue #38
    Signed-off-by: Valina Li <valina.li@intel.com>

--- benchmark-scripts/format_avc_mp4.sh
+++ benchmark-scripts/format_avc_mp4.sh
@@ -20,33 +20,10 @@ WIDTH=3840
 HEIGHT=2160
 FPS=15
 
-source ../get-gpu-info.sh
-
 if [ -z "$2" ]
 then
         show_help
         exit 1
-fi
-
-
-result=${1/.mp4/"-bench.mp4"}
-if [ -f ../sample-media/$result ]
-then
-	echo "Skipping...conversion was already done for ../sample-media/$result."
-	exit 0
-fi
-
-if [ ! -f ../sample-media/$1 ] && [ ! -f ../sample-media/$result ]
-then	
-	wget -O ../sample-media/$1 $2
-fi
-
-if [ ! -f ../sample-media/$1 ]
-then
-	echo "ERROR: Can not find video file or 
-	"
-	show_help
-	exit 1
 fi
 
 if [ ! -z "$3" ]
@@ -64,21 +41,57 @@ then
         FPS=$5
 fi
 
-if [ -z "$WIDTH" ] || [ -z "$HEIGHT" ] || [ -z "$FPS" ]
+if ! [[ "$WIDTH" =~ ^[0-9]+$ ]]
 then
-	echo "ERROR: Invalid width height fps"
+	echo "ERROR: width should be integer."
 	exit 1
 fi
 
-
-if [ $HAS_FLEX_140 == 1 ] || [ $HAS_FLEX_170 == 1 ] || [ $HAS_ARC == 1 ]
+if ! [[ "$HEIGHT" =~ ^[0-9]+$ ]]
 then
-        TAG=sco-dgpu:2.0
+	echo "ERROR: height should be integer."
+	exit 1
+fi
 
+if ! [[ "$FPS" =~ ^[0-9]+(\.[0-9]+)*$ ]]
+then
+	echo "ERROR: FPS should be number."
+	exit 1
+fi
+
+result=${1/.mp4/"-$WIDTH-$FPS-bench.mp4"}
+if [ -f ../sample-media/$result ]
+then
+	echo "Skipping...conversion was already done for ../sample-media/$result."
+	exit 0
+fi
+
+FIND_IMAGE_SOC=$(docker images | grep "sco-soc")
+FIND_IMAGE_DGPU=$(docker images | grep "sco-dgpu")
+if [ -z "$FIND_IMAGE_SOC" ] && [ -z "$FIND_IMAGE_DGPU" ]
+then
+	echo "ERROR: Can not find docker image sco-soc or sco-dgpu, please build image first!"
+	exit 1
+elif [ ! -z "$FIND_IMAGE_SOC" ]
+then
+	TAG=sco-dgpu:2.0
 else
-        echo "ERROR: Requires Intel Flex/Arc GPU"
+	TAG=sco-soc:2.0
+fi 
+
+if [ ! -f ../sample-media/$1 ] && [ ! -f ../sample-media/$result ]
+then	
+	wget -O ../sample-media/$1 $2
+fi
+
+if [ ! -f ../sample-media/$1 ]
+then
+	echo "ERROR: Can not find video file or 
+	"
+	show_help
 	exit 1
 fi
+
 
 echo "$WIDTH $HEIGHT $FPS"
 docker run --network host --privileged --user root --ipc=host -e VIDEO_FILE=$1 -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`/../sample-media/:/vids -w /vids -it  --rm $TAG bash -c "if [ -f /vids/$result ]; then exit 1; else gst-launch-1.0 filesrc location=/vids/$1 ! qtdemux ! h264parse ! vaapih264dec ! vaapipostproc width=$WIDTH height=$HEIGHT ! videorate ! 'video/x-raw, framerate=$FPS/1' ! vaapih264enc ! h264parse ! mp4mux ! filesink location=/vids/$result; fi"
---
+++ benchmark-scripts/test_format_avc_mp4.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 Intel Corporation.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+
+# test setup: prepare test folder for download test file
+testFolder=../sample-media/test
+FILENAME_DOWNLOAD=test/test.mp4
+DEFAULT_FILE_PATH_NAME=../sample-media/test/test-3840-15-bench.mp4
+FILE_URL_TO_DOWNLOAD=https://storage.openvinotoolkit.org/data/test_data/videos/smartlab/v3/stream_1_left.mp4
+mkdir $testFolder
+
+cleanupTestFolder() {
+  echo 
+  echo "remove test folder..."
+  rm -rf "$testFolder"
+  echo "done."
+}
+
+cleanupTestFolderContent() {
+    echo "remove all files under test folder..."
+    # remove downloaded files so it's re-testable
+    rm -f "$testFolder/*"
+}
+
+# test case 1: test without image
+echo
+echo "# test case 1: test without image"
+docker image tag sco-soc:2.0 test-soc:2.0
+docker image tag sco-dgpu:2.0 test-dgpu:2.0
+docker rmi sco-soc:2.0
+docker rmi sco-dgpu:2.0
+
+FIND_IMAGE_SCO=$(docker images --format "{{.Repository}}" | grep "sco-")
+
+output=$(./format_avc_mp4.sh $FILENAME_DOWNLOAD $FILE_URL_TO_DOWNLOAD) 
+statusCode=$?
+if [ -z "$FIND_IMAGE_SCO" ]
+then
+    if [ $statusCode==1 ]
+    then
+        echo "test PASSED: test without image and got expected status code of 1"
+    else
+        echo "test FAILED: expecting status code 1, but got something else"
+    fi
+else
+    echo "test FAILED: Image found"
+fi
+# rename back the images
+docker image tag test-soc:2.0 sco-soc:2.0
+docker image tag test-dgpu:2.0 sco-dgpu:2.0
+docker rmi test-soc:2.0
+docker rmi test-dgpu:2.0
+cleanupTestFolderContent
+
+# test case 2: test with image, got statusCode 0 and test media file downloaded (happy path)
+echo
+echo "# test case 2: test with image, got statusCode 0 and test media file downloaded (happy path)"
+output=$(./format_avc_mp4.sh $FILENAME_DOWNLOAD $FILE_URL_TO_DOWNLOAD)  
+statusCode=$?
+echo "$statusCode"
+if [ $statusCode==0 ]
+then
+    if [ -f "$DEFAULT_FILE_PATH_NAME" ]
+    then
+        echo "test PASSED: $DEFAULT_FILE_PATH_NAME has been downloaded for $FILENAME_DOWNLOAD with $FILE_URL_TO_DOWNLOAD."
+    else
+        echo "test FAILED: $DEFAULT_FILE_PATH_NAME has NOT been downloaded for $FILENAME_DOWNLOAD with $FILE_URL_TO_DOWNLOAD."
+    fi
+else
+    echo "test FAILED: expecting status code 0 but found $statusCode for $FILENAME_DOWNLOAD with $FILE_URL_TO_DOWNLOAD."
+fi
+cleanupTestFolderContent
+
+# test case 3: download 2nd time, expect message "Skipping..."
+SUB="Skipping..."
+echo
+echo "# test case 3: download 2nd time, expect message \"$SUB\""
+output=$(./format_avc_mp4.sh $FILENAME_DOWNLOAD $FILE_URL_TO_DOWNLOAD)  
+statusCode=$?
+if [ $statusCode==0 ]
+then
+    if test -f "$DEFAULT_FILE_PATH_NAME"; then
+        #download again
+        output=$(./format_avc_mp4.sh $FILENAME_DOWNLOAD $FILE_URL_TO_DOWNLOAD)  
+        statusCode=$?
+        if [ $statusCode==0 ]
+        then
+            if [[ "$output" == "$SUB"* ]]
+            then
+                echo "test PASSED: Second time download was skipped!"
+            else
+                echo "test FAILED: Second time download was missing skipped message!"
+            fi
+        else
+            echo "test FAILED: Second time download ERROR: $output: expecting status code 0 but found $statusCode."
+        fi
+    else
+        echo "test FAILED: download $FILENAME_DOWNLOAD first time failed"
+    fi
+else
+    echo "test FAILED: download $FILENAME_DOWNLOAD first time failed: $output: expecting status code 0 but found $statusCode."
+fi
+cleanupTestFolderContent
+
+
+# test case 4: input resize, expect file name with resize in the file name (happy path)
+echo
+echo "# test case 4: input resize, expect file name with resize in the file name (happy path)"
+WIDTH=1080
+HEIGHT=720
+FPS=10
+output=$(./format_avc_mp4.sh $FILENAME_DOWNLOAD $FILE_URL_TO_DOWNLOAD $WIDTH $HEIGHT $FPS)  
+statusCode=$?
+FILE_PATH_NAME=../sample-media/test/test-$WIDTH-$FPS-bench.mp4
+if [ $statusCode==0 ]
+then
+    if test -f "$FILE_PATH_NAME"; then
+        echo "test PASSED: with input width $WIDTH, height $HEIGHT, FPS $FPS."
+    else
+        echo "test FAILED: with input width $WIDTH, height $HEIGHT, FPS $FPS: couldn't find downloaded file $FILE_PATH_NAME."
+    fi
+else
+    echo "test FAILED: with input width $WIDTH, height $HEIGHT, FPS $FPS: expecting status code 0 but found $statusCode."
+fi
+cleanupTestFolderContent
+
+# test case 5: input Width should be integer type
+echo
+echo "# test case 5: input Width should be integer type"
+WIDTH=8abv
+HEIGHT=720
+FPS=10
+EXPECTED_MESSAGE="ERROR: width should be integer."
+output=$(./format_avc_mp4.sh $FILENAME_DOWNLOAD $FILE_URL_TO_DOWNLOAD, $WIDTH $HEIGHT $FPS)
+statusCode=$?
+if [ $statusCode==1 ]
+then
+    if [[ "$output" == "$EXPECTED_MESSAGE" ]]
+    then
+        echo "test PASSED: check for invalid width input"
+    else
+        echo "test FAILED: check for invalid width input"
+    fi
+else
+    echo "test FAILED: check for invalid width input, it should return status code of 1"
+fi
+cleanupTestFolderContent
+
+# test case 6: input Height should be integer type
+echo
+echo "# test case 6: input Height should be integer type"
+WIDTH=1035
+HEIGHT=8.0fd
+FPS=10
+EXPECTED_MESSAGE="ERROR: height should be integer."
+output=$(./format_avc_mp4.sh $FILENAME_DOWNLOAD $FILE_URL_TO_DOWNLOAD, $WIDTH $HEIGHT $FPS)
+statusCode=$?
+if [ $statusCode==1 ]
+then
+    if [[ "$output" == "$EXPECTED_MESSAGE" ]]
+    then
+        echo "test PASSED: check for invalid height input"
+    else
+        echo "test FAILED: check for invalid height input"
+    fi
+else
+    echo "test FAILED: check for invalid height input, it should return status code of 1"
+fi
+cleanupTestFolderContent
+
+# test case 7: input FPS should be float or integer type
+echo
+echo "# test case 7: input FPS should be float or integer type"
+WIDTH=1035
+HEIGHT=335
+FPS=a.09
+EXPECTED_MESSAGE="ERROR: FPS should be number."
+output=$(./format_avc_mp4.sh $FILENAME_DOWNLOAD $FILE_URL_TO_DOWNLOAD, $WIDTH $HEIGHT $FPS)
+statusCode=$?
+if [ $statusCode==1 ]
+then
+    if [[ "$output" == "$EXPECTED_MESSAGE" ]]
+    then
+        echo "test PASSED: check for invalid FPS input"
+    else
+        echo "test FAILED: check for invalid FPS input"
+    fi
+else
+    echo "test FAILED: check for invalid FPS input, it should return status code of 1"
+fi
+cleanupTestFolderContent
+
+# clean up: remove the test folder
+cleanupTestFolder
+
+
+
+
