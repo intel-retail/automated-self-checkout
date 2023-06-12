1572471b640effcdead0104dc163280fc753f034
Valina Li <valina.li@intel.com>
2023-06-12 14:38:34 -0700

    fix: fixed docker-run.sh with wrong message when no --workload option
    Closes: #75
    Signed-off-by: Valina Li <valina.li@intel.com>

--- docker-run.sh
+++ docker-run.sh
@@ -6,13 +6,12 @@
 #
 
 source benchmark-scripts/get-gpu-info.sh
+WORKLOAD_SCRIPT="docker-run-dlstreamer.sh"
 
 if [ -z "$PLATFORM" ] || [ -z "$INPUTSRC" ]
 then
 	source get-options.sh "$@"
 fi
 
-#todo figure out how to have the workload script see the env variables set by get-options.sh so that it doesn't have to be run twice. 
-# or modify this script to determine the workload script
 echo "running $WORKLOAD_SCRIPT"
 ./$WORKLOAD_SCRIPT "$@"
