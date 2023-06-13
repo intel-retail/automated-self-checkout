295da9fef936bf3fbdd7982016ecad0549918f62
Valina Li <valina.li@intel.com>
2023-06-13 11:12:16 -0700

    feat: Add model download top level script and add unit test for ovms … (#86)
    * feat: Add model download top level script and add unit test for ovms model download
    Signed-off-by: Valina Li <valina.li@intel.com>

---
+++ download_models/downloadOVMSModels.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 Intel Corporation.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Todo: update document for how we get the bit model
+# Here is detail about getting bit model: https://medium.com/openvino-toolkit/accelerate-big-transfer-bit-model-inference-with-intel-openvino-faaefaee8aec
+
+pipelineZooModel="https://storage.openvinotoolkit.org/repositories/open_model_zoo/2022.3/models_bin/1/"
+segmentation="instance-segmentation-security-1040"
+modelPrecisionFP16INT8=FP16-INT8
+localModelFolderName="instance_segmentation_omz_1040"
+
+REFRESH_MODE=0
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --refresh)
+            echo "running model downloader in refresh mode"
+            REFRESH_MODE=1
+            ;;
+        *)
+            echo "Invalid flag: $1" >&2
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+modelDir="../configs/opencv-ovms/models/2022/"
+mkdir -p $modelDir
+cd $modelDir || { echo "Failure to cd to $modelDir"; exit 1; }
+
+if [ "$REFRESH_MODE" -eq 1 ]; then
+    # cleaned up all downloaded files so it will re-download all files again
+    rm "${PWD}/$localModelFolderName/$modelPrecisionFP16INT8/1/$segmentation.bin" || true
+    rm "${PWD}/$localModelFolderName/$modelPrecisionFP16INT8/1/$segmentation.xml" || true
+    rm -rf $localModelFolderName  || true
+    rm -rf BiT_M_R50x1_10C_50e_IR  || true
+fi
+
+segmentationModelFile="$localModelFolderName/$modelPrecisionFP16INT8/1/$segmentation.bin"
+echo $segmentationModelFile
+if [ -f "$segmentationModelFile" ]; then
+    echo "models already exists, skip downloading..."
+    exit 0
+fi
+
+mkdir -p "BiT_M_R50x1_10C_50e_IR/FP16-INT8/1"
+mkdir -p "$localModelFolderName/FP16-INT8/1"
+
+# $1 model file name
+# $2 download URL
+# $3 model percision
+# $4 local model folder name
+getOVMSModelFiles() {
+    # Make model directory
+    mkdir -p $4/$3/1
+    
+    # Get the models
+    wget $2/$3/$1".bin" -P $4/$3/1
+    wget $2/$3/$1".xml" -P $4/$3/1
+}
+
+getOVMSModelFiles $segmentation $pipelineZooModel$segmentation $modelPrecisionFP16INT8 $localModelFolderName
---
+++ download_models/testDownloadOVMSModels.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 Intel Corporation.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+testModelDownload() {
+    if [ -d "$1" ]; then
+      # Take action if $DIR exists. #
+      # check if there is a 2nd parameter input as the filename
+      if [ ! -z "$2" ]; then
+        filePath="$1"/"$2"
+        if [ -f "$filePath" ]; then
+          echo "Passed: found file $filePath"
+        else
+          echo "Failed: expected file not found $filePath"
+          exit 1
+        fi
+      else
+        echo "Passed: found folder $1"
+      fi
+    else
+      echo "Failed: expected folder not found $1"
+      exit 1
+    fi
+}
+
+cleanupOVMSDownload() {
+  echo 
+  echo "cleaning up opencv-ovms download files..."
+  # cleaned up all downloaded files so it will re-download all files again
+  rm "../configs/opencv-ovms/models/2022/$localModelFolderName/$modelPrecisionFP16INT8/1/$segmentation.bin" || true
+  rm "../configs/opencv-ovms/models/2022/$localModelFolderName/$modelPrecisionFP16INT8/1/$segmentation.xml" || true
+  rm -rf "../configs/opencv-ovms/models/2022/$localModelFolderName" || true
+  rm -rf "../configs/opencv-ovms/models/2022/BiT_M_R50x1_10C_50e_IR" || true
+
+  echo "done clean up OVMS download."
+}
+
+echo
+echo "CASE Test 1: happy path"
+#download ovms models downloaded
+./downloadOVMSModels.sh
+
+modelOVMSDir="../configs/opencv-ovms/models/2022/"
+localModelFolderName="instance_segmentation_omz_1040"
+modelPrecisionFP16INT8=FP16-INT8
+segmentation="instance-segmentation-security-1040"
+
+# Test Segmentation download
+expectedSegmentationDir=$modelOVMSDir$localModelFolderName
+echo "$expectedSegmentationDir"
+# Test instance-segmentation-security-1040.bin model file exists
+testModelDownload "$expectedSegmentationDir/$modelPrecisionFP16INT8/1" "$segmentation.xml"
+expectedSegmentationModelFile="$expectedSegmentationDir/$modelPrecisionFP16INT8/1/$segmentation.xml"
+if [ -f "$expectedSegmentationModelFile" ]; then
+  echo "Passed: found ${expectedSegmentationModelFile}"
+else
+  echo "Failed: expect model file not found ${expectedSegmentationModelFile}"
+  exit 1
+fi
+
+timestamp_ovms_model=$(stat -c %Z "$expectedSegmentationModelFile")
+
+echo 
+echo "CASE Test 2: re-run downloadOVMSModels and it should not re-download without --refresh option"
+./downloadOVMSModels.sh
+timestamp_model_rerun=$(stat -c %Z "$expectedSegmentationModelFile")
+if [ "$timestamp_model_rerun" -eq "$timestamp_ovms_model" ]; then
+  echo "Passed: re-run downloadOVMSModels and it didn't re-download files"
+else
+  echo "Failed: re-run downloadOVMSModels and it re-download files"
+  cleanupOVMSDownload
+  exit 1
+fi
+
+echo
+echo "CASE Test 3: --refresh option"
+# use refresh option to re-test:
+./downloadOVMSModels.sh --refresh
+if [ -f "$expectedSegmentationModelFile" ]; then
+  refresh_timestamp_ovms_model=$(stat -c %Z "$expectedSegmentationModelFile")
+  if [ "$refresh_timestamp_ovms_model" -gt "$timestamp_ovms_model" ]; then
+    echo "Passed: --refresh option test found ${expectedSegmentationModelFile} and timestamp refreshed"
+  else
+    echo "Failed: --refresh option test found ${expectedSegmentationModelFile} but timestamp not refreshed"
+    cleanupOVMSDownload
+    exit 1
+  fi
+else
+  echo "Failed: --refresh option test expect model file not found ${expectedSegmentationModelFile}"
+  cleanupOVMSDownload
+  exit 1
+fi
+
+cleanupOVMSDownload
