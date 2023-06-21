b49857c4c1e94acf9f730ff2436d5a07815510d3
Jim Wang <yutsung.jim.wang@intel.com>
2023-06-21 11:11:16 -0700

    fix: update license to apache-2.0
    Closes: #129
    
    Signed-off-by: Jim Wang <yutsung.jim.wang@intel.com>

--- clean-containers.sh
+++ clean-containers.sh
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2023 Intel Corporation.
 #
-# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-License-Identifier: Apache-2.0
 #
 
 mapfile -t containers < <(docker ps  --filter="name=$1" -q -a)
