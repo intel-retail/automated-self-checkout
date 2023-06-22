b8696285e3c55603974bc1509ceaed91310cdad0
Brian McGinn <brian.mcginn@intel.com>
2023-06-22 14:58:32 -0700

    fix: add to the output file name

--- benchmark-scripts/smoke_test.sh
+++ benchmark-scripts/smoke_test.sh
@@ -22,35 +22,35 @@ then
 fi
 
 sudo rm -rf results || true
-sudo rm -rf "platform_$PLATFORM"_"cpuonly_$CPU_ONLY"_smoke_test_camera_simulator_full/  || true
-sudo rm -rf "platform_$PLATFORM"_"cpuonly_$CPU_ONLY"_smoke_test_camera_simulator_yolov5_classification/  || true
-sudo rm -rf "platform_$PLATFORM"_"cpuonly_$CPU_ONLY"_smoke_test_camera_simulator_yolov5/  || true
-sudo rm -rf "platform_$PLATFORM"_"cpuonly_$CPU_ONLY"_smoke_test_file_full/ || true
-sudo rm -rf "platform_$PLATFORM"_"cpuonly_$CPU_ONLY"_smoke_test_stream_density/ || true
+sudo rm -rf platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_camera_simulator_full/  || true
+sudo rm -rf platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_camera_simulator_yolov5_classification/  || true
+sudo rm -rf platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_camera_simulator_yolov5/  || true
+sudo rm -rf platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_file_full/ || true
+sudo rm -rf platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_stream_density/ || true
 
 # Camera simulator full pipeline
-sudo CPU_ONLY=$CPU_ONLY ./benchmark.sh --pipelines 1 --logdir "$PLATFORM"_"$CPU_ONLY"_smoke_test_camera_simulator_full/data --init_duration 30 --duration 60 --platform $PLATFORM --inputsrc rtsp://127.0.0.1:8554/camera_0
+sudo CPU_ONLY=$CPU_ONLY ./benchmark.sh --pipelines 1 --logdir platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_camera_simulator_full/data --init_duration 30 --duration 60 --platform $PLATFORM --inputsrc rtsp://127.0.0.1:8554/camera_0
 # consolidate results
-make consolidate ROOT_DIRECTORY="$PLATFORM"_"$CPU_ONLY"_smoke_test_camera_simulator_full
+make consolidate ROOT_DIRECTORY=platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_camera_simulator_full
 
 
 # Camera simulator Yolov5 and classification only
-sudo CPU_ONLY=$CPU_ONLY  ./benchmark.sh --pipelines 1 --logdir "$PLATFORM"_"$CPU_ONLY"_smoke_test_camera_simulator_yolov5_classification/data --init_duration 30 --duration 60 --platform $PLATFORM --inputsrc rtsp://127.0.0.1:8554/camera_0 --ocr_disabled --barcode_disabled
+sudo CPU_ONLY=$CPU_ONLY  ./benchmark.sh --pipelines 1 --logdir platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_camera_simulator_yolov5_classification/data --init_duration 30 --duration 60 --platform $PLATFORM --inputsrc rtsp://127.0.0.1:8554/camera_0 --ocr_disabled --barcode_disabled
 # consolidate results
-make consolidate ROOT_DIRECTORY="$PLATFORM"_"$CPU_ONLY"_smoke_test_camera_simulator_yolov5_classification
+make consolidate ROOT_DIRECTORY=platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_camera_simulator_yolov5_classification
 
 
 # Camera simulator yolov5 only
-sudo CPU_ONLY=$CPU_ONLY ./benchmark.sh --pipelines 1 --logdir "$PLATFORM"_"$CPU_ONLY"_smoke_test_camera_simulator_yolov5/data --init_duration 30 --duration 60 --platform $PLATFORM --inputsrc rtsp://127.0.0.1:8554/camera_0 --ocr_disabled --barcode_disabled --classification_disabled
+sudo CPU_ONLY=$CPU_ONLY ./benchmark.sh --pipelines 1 --logdir platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_camera_simulator_yolov5/data --init_duration 30 --duration 60 --platform $PLATFORM --inputsrc rtsp://127.0.0.1:8554/camera_0 --ocr_disabled --barcode_disabled --classification_disabled
 # consolidate results
-make consolidate ROOT_DIRECTORY="$PLATFORM"_"$CPU_ONLY"_smoke_test_camera_simulator_yolov5
+make consolidate ROOT_DIRECTORY=platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_camera_simulator_yolov5
 
 
 # File full pipeline
-sudo CPU_ONLY=$CPU_ONLY ./benchmark.sh --pipelines 1 --logdir "$PLATFORM"_"$CPU_ONLY"_smoke_test_file_full/data --init_duration 5 --duration 20 --platform $PLATFORM --inputsrc file:coca-cola-4465029-3840-15-bench.mp4
+sudo CPU_ONLY=$CPU_ONLY ./benchmark.sh --pipelines 1 --logdir platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_file_full/data --init_duration 5 --duration 20 --platform $PLATFORM --inputsrc file:coca-cola-4465029-3840-15-bench.mp4
 # consolidate results
-make consolidate ROOT_DIRECTORY="$PLATFORM"_"$CPU_ONLY"_smoke_test_file_full
+make consolidate ROOT_DIRECTORY=platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_file_full
 
 
 # Stream density
-sudo CPU_ONLY=$CPU_ONLY ./benchmark.sh --stream_density 60 --logdir "$PLATFORM"_"$CPU_ONLY"_smoke_test_stream_density/data --init_duration 30 --duration 60 --platform $PLATFORM --inputsrc rtsp://127.0.0.1:8554/camera_0
+sudo CPU_ONLY=$CPU_ONLY ./benchmark.sh --stream_density 60 --logdir platform_"$PLATFORM"_cpuonly_"$CPU_ONLY"_smoke_test_stream_density/data --init_duration 30 --duration 60 --platform $PLATFORM --inputsrc rtsp://127.0.0.1:8554/camera_0
