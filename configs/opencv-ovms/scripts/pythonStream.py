b368b8b0475d1e537bb61f4a9c64c5e13ee59270
Brian McGinn <brian.mcginn@intel.com>
2023-06-29 16:26:50 -0700

    feat: temp pythonStream.py example
    Signed-off-by: Brian McGinn <brian.mcginn@intel.com>

---
+++ configs/opencv-ovms/scripts/pythonStream.py
@@ -0,0 +1,26 @@
+import cv2
+import os
+
+RTSP_URL = 'rtsp://192.168.0.246:8554/camera_0'
+
+os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
+
+cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
+
+if not cap.isOpened():
+    print('Cannot open RTSP stream')
+    exit(-1)
+
+while True:
+    _, frame = cap.read()
+    width = 608
+    height = 608
+    img = cv2.resize(frame, (width, height))
+    # img = img.transpose(2,0,1).reshape(1,3,height,width)
+    cv2.imshow('RTSP stream', img)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
