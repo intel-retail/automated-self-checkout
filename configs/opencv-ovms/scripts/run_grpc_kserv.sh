b4b1f33c9e9fb053133263b21cc9734646b138b3
Jim Wang <yutsung.jim.wang@intel.com>
2023-07-13 08:42:59 -0700

    fix: fixed typo and add comments for DEBUG env
    Signed-off-by: Jim Wang <yutsung.jim.wang@intel.com>

--- configs/opencv-ovms/scripts/run_grpc_kserv.sh
+++ configs/opencv-ovms/scripts/run_grpc_kserv.sh
@@ -31,15 +31,14 @@ done
 
 
 
-echo "running grpcpython with GRPC_PORT=$GRPC_PORT"
-
-echo "TECTION_MODEL_NAME:$DETECTION_MODEL_NAME"
+echo "running grpcpython with GRPC_PORT=$GRPC_PORT, DETECTION_MODEL_NAME:$DETECTION_MODEL_NAME"
 
 # /scripts is mounted during the docker run 
 # python3 /scripts/grpc_infer_binary_maskrcnn-omz.py --images_list /images/inputimages.txt --grpc_address 127.0.0.1 --grpc_port $GRPC_PORT --input_name image  --batchsize $BATCH_SIZE --model_name  instance-segmentation-security-1040 2>&1
 # python3 /scripts/grpc_infer_binary_bit.py --images_list /images/inputimages.txt --grpc_address 127.0.0.1 --grpc_port $GRPC_PORT --input_name input_1  --batchsize $BATCH_SIZE --model_name  bit_64
 if [ ! -z "$DEBUG" ]
 then
+	# when there is non-empty DEBUG env, the output of app outputs to the console for easily debugging
 	python3 /scripts/grpc_python.py --input_src $inputsrc --grpc_address 127.0.0.1 --grpc_port $GRPC_PORT --model_name  "$DETECTION_MODEL_NAME"
 else
 	python3 /scripts/grpc_python.py --input_src $inputsrc --grpc_address 127.0.0.1 --grpc_port $GRPC_PORT --model_name  "$DETECTION_MODEL_NAME" 2>&1  | tee >/tmp/results/r$cid_count.jsonl >(stdbuf -oL sed -n -e 's/^.*fps: //p' | stdbuf -oL cut -d , -f 1 > /tmp/results/pipeline$cid_count.log)
