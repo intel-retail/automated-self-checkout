035ebf9d3315fa87523b933a96b6d9f264f6672c
Brian McGinn <brian.mcginn@intel.com>
2023-07-19 08:01:17 -0700

    fix: Make scripts executable
    Signed-off-by: Brian McGinn <brian.mcginn@intel.com>

--- configs/dlstreamer/framework-pipelines/yolov5_pipeline/yolov5s.sh
+++ configs/dlstreamer/framework-pipelines/yolov5_pipeline/yolov5s.sh
@@ -5,10 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DECODE= "vaapidecodebin" 
-DEVICE= "CPU" #GPU|CPU|MULTI:GPU,CPU
-PRO_PROCESS= "" #""|pre-process-backend=vaapi-surface-sharing pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1 
-AGGREGATE= "gvametaaggregate name=aggregate !" #""|gvametaaggregate name=aggregate !
+DECODE="vaapidecodebin" 
+DEVICE="CPU" #GPU|CPU|MULTI:GPU,CPU
+PRE_PROCESS="" #""|pre-process-backend=vaapi-surface-sharing|pre-process-backend=vaapi-surface-sharing pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1 
+AGGREGATE="gvametaaggregate name=aggregate !" #""|gvametaaggregate name=aggregate !
 
 while :; do
     case $1 in
@@ -16,15 +16,16 @@ while :; do
         show_help
         exit
         ;;
-    --decode) #vaapidecodebin|decodebin 
+    --decode)
         if [ "$2" ]; then
             DECODE=$2
             shift
         else
-            error 'ERROR: "--decode" requires an argument vaapidecodebin|decodebin.'
+            echo "using default"
+            # error 'ERROR: "--decode" requires an argument vaapidecodebin|decodebin.'
         fi
         ;;
-    --device) #GPU|CPU|MULTI:GPU,CPU
+    --device)
         if [ "$2" ]; then
             DEVICE=$2
             if [ -z "$LOW_POWER" ]; then
@@ -32,24 +33,30 @@ while :; do
             fi
             shift
         else
-            error 'ERROR: "--decode" requires an argument GPU|CPU|MULTI:GPU,CPU.'
+            echo "using default"
+            # error 'ERROR: "--decode" requires an argument GPU|CPU|MULTI:GPU,CPU.'
         fi
         ;;
-    --pre_process) #""|pre-process-backend=vaapi-surface-sharing pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1  
+    --pre_process) 
         if [ "$2" ]; then
-            PRO_PROCESS=$2
+            PRE_PROCESS=$2
             shift
         else
-            error 'ERROR: "--decode" requires an argument ""|pre-process-backend=vaapi-surface-sharing pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1.'
+            echo "using default"
+            # error 'ERROR: "--decode" requires an argument ""|pre-process-backend=vaapi-surface-sharing|pre-process-backend=vaapi-surface-sharing pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1 .'
         fi
         ;;
-    --aggregate) #""|pre-process-backend=vaapi-surface-sharing pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1  
+    --aggregate)
         if [ "$2" ]; then
             AGGREGATE=$2
             shift
         else
-            error 'ERROR: "--decode" requires an argument ""|gvametaaggregate name=aggregate !.'
+            echo "using default"
+            # error 'ERROR: "--decode" requires an argument ""|gvametaaggregate name=aggregate !.'
         fi
+        ;;
+    --render_mode)  
+        RENDER_MODE=1
         ;;
     -?*)
         error "ERROR: Unknown option $1"
@@ -77,12 +84,13 @@ done
 # fi
 
 if [ "$RENDER_MODE" == "1" ]; then
-	gst-launch-1.0 $inputsrc ! $DECODE ! gvadetect model-instance-id=odmodel name=detection model=models/yolov5s/1/FP32-INT8/yolov5s.xml model-proc=models/yolov5s/1/yolov5s.json threshold=.5 device=$DEVICE $PRO_PROCESS ! $AGGREGATE gvametaconvert name=metaconvert add-empty-results=true ! gvametapublish name=destination file-format=2 file-path=/tmp/results/r$cid_count.jsonl  \
+	gst-launch-1.0 $inputsrc ! $DECODE ! gvadetect model-instance-id=odmodel name=detection model=models/yolov5s/1/FP32-INT8/yolov5s.xml model-proc=models/yolov5s/1/yolov5s.json threshold=.5 device=$DEVICE $PRE_PROCESS ! $AGGREGATE gvametaconvert name=metaconvert add-empty-results=true ! gvametapublish name=destination file-format=2 file-path=/tmp/results/r$cid_count.jsonl  \
 	! videoconvert ! video/x-raw,format=I420 \
 	! gvawatermark ! videoconvert ! fpsdisplaysink video-sink=ximagesink sync=true --verbose \
 	2>&1 | tee >/tmp/results/gst-launch_$cid_count.log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /tmp/results/pipeline$cid_count.log)
 else
-    gst-launch-1.0 $inputsrc ! $DECODE ! gvadetect model-instance-id=odmodel name=detection model=models/yolov5s/1/FP32-INT8/yolov5s.xml model-proc=models/yolov5s/1/yolov5s.json threshold=.5 device=$DEVICE $PRO_PROCESS ! $AGGREGATE gvametaconvert name=metaconvert add-empty-results=true ! gvametapublish name=destination file-format=2 file-path=/tmp/results/r$cid_count.jsonl ! fpsdisplaysink video-sink=fakesink sync=true --verbose 2>&1 | tee >/tmp/results/gst-launch_$cid_count.log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /tmp/results/pipeline$cid_count.log)
+    echo "run pipeline"
+    gst-launch-1.0 $inputsrc ! $DECODE ! gvadetect model-instance-id=odmodel name=detection model=models/yolov5s/1/FP32-INT8/yolov5s.xml model-proc=models/yolov5s/1/yolov5s.json threshold=.5 device=$DEVICE $PRE_PROCESS ! $AGGREGATE gvametaconvert name=metaconvert add-empty-results=true ! gvametapublish name=destination file-format=2 file-path=/tmp/results/r$cid_count.jsonl ! fpsdisplaysink video-sink=fakesink sync=true --verbose 2>&1 | tee >/tmp/results/gst-launch_$cid_count.log >(stdbuf -oL sed -n -e 's/^.*current: //p' | stdbuf -oL cut -d , -f 1 > /tmp/results/pipeline$cid_count.log)
 fi
 
 #  dgpu
