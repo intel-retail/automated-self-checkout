d45f7e0a76a7df6af576ca873a495c1f91c3e7fa
Brian McGinn <brian.mcginn@intel.com>
2023-07-20 15:51:17 -0700

    fix: Add yolov5 model download for OVMS
    Signed-off-by: Brian McGinn <brian.mcginn@intel.com>

--- download_models/downloadOVMSModels.sh
+++ download_models/downloadOVMSModels.sh
@@ -70,3 +70,78 @@ then
 fi
 
 docker run -it --rm -v "$modelDir"/BiT_M_R50x1_10C_50e_IR/FP16-INT8/1:/result bit_model_downloader:dev
+
+
+
+
+
+
+
+
+
+
+
+modelPrecisionFP16INT8=FP16-INT8
+
+pipelineZooModel="https://github.com/dlstreamer/pipeline-zoo-models/raw/main/storage/"
+
+# $1 model file name
+# $2 download URL
+# $3 model percision
+getModelFiles() {
+    # Make model directory
+    # ex. kdir efficientnet-b0/1/FP16-INT8
+    mkdir -p $1/1/$3
+    
+    # Get the models
+    wget $2/$3/$1".bin" -P $1/1/$3
+    wget $2/$3/$1".xml" -P $1/1/$3
+}
+
+# $1 model file name
+# $2 download URL
+# $3 process file name (this can be different than the model name ex. horizontal-text-detection-0001 is using horizontal-text-detection-0002.json)
+getProcessFile() {
+    # Get process file
+    wget $2/$3.json -P $1/1
+}
+
+REFRESH_MODE=0
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --refresh)
+            echo "running model downloader in refresh mode"
+            REFRESH_MODE=1
+            ;;
+        *)
+            echo "Invalid flag: $1" >&2
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+yolov5s="yolov5s"
+
+
+if [ "$REFRESH_MODE" -eq 1 ]; then
+    # we don't delete the whole directory as there are some exisitng checked-in files
+    rm "${PWD}/$yolov5s/1/FP16-INT8/yolov5s.bin" || true
+    rm "${PWD}/$yolov5s/1/FP16-INT8/yolov5s.xml" || true
+    rm "${PWD}/$yolov5s/1/yolov5s.json" || true
+fi
+
+# Yolov5
+# checking whether the model file .bin already exists or not before downloading
+yolov5ModelFile="${PWD}/$yolov5s/$modelPrecisionFP16INT8/1/$yolov5s.bin"
+echo $yolov5ModelFile
+if [ -f "$yolov5ModelFile" ]; then
+    echo "models already exists, skip downloading..."
+    exit 0
+fi
+
+echo "Downloading models..."
+
+# Yolov5s INT8
+getModelFiles $yolov5s $pipelineZooModel"yolov5s-416_INT8" $modelPrecisionFP16INT8
+getProcessFile $yolov5s $pipelineZooModel"yolov5s-416" $yolov5s
