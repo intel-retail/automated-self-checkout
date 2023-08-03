803e8d939b218185e169b1686ff9cceaeba0d1ec
brian-intel <brian.mcginn@intel.com>
2023-08-03 15:44:26 -0700

    feat: Add DLStreamer profile (#201)
    * feat: Add DLStreamer profile
    
    Signed-off-by: Brian McGinn <brian.mcginn@intel.com>
    
    * fix: Remove stream density from profile for the current release
    
    Signed-off-by: Brian McGinn <brian.mcginn@intel.com>
    
    ---------
    
    Signed-off-by: Brian McGinn <brian.mcginn@intel.com>

---
+++ configs/opencv-ovms/scripts/run_gst.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 Intel Corporation.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+echo "Run gst pipeline"
+rmDocker=--rm
+if [ ! -z "$DEBUG" ]
+then
+	# when there is non-empty DEBUG env, the output of app outputs to the console for easily debugging
+	rmDocker=""
+fi
+
+echo $rmDocker
+TAG=sco-soc:2.0
+pipeline="yolov5s.sh"
+
+echo $PLATFORM
+
+bash_cmd="/home/pipeline-server/framework-pipelines/$PLATFORM/$pipeline"
+
+if grep -q "rtsp" <<< "$inputsrc"; then
+	# rtsp
+	inputsrc=$inputsrc" ! rtph264depay "
+fi
+
+docker run --network host $cameras $TARGET_USB_DEVICE $TARGET_GPU_DEVICE --user root --ipc=host --name automated-self-checkout$cid_count -e RENDER_MODE=$RENDER_MODE $stream_density_mount -e INPUTSRC_TYPE=$INPUTSRC_TYPE -e DISPLAY=$DISPLAY -e cl_cache_dir=/home/pipeline-server/.cl-cache -v $cl_cache_dir:/home/pipeline-server/.cl-cache -v /tmp/.X11-unix:/tmp/.X11-unix -v $RUN_PATH/sample-media/:/home/pipeline-server/vids -v $RUN_PATH/configs/dlstreamer/pipelines:/home/pipeline-server/pipelines -v $RUN_PATH/configs/dlstreamer/extensions:/home/pipeline-server/extensions -v $RUN_PATH/results:/tmp/results -v $RUN_PATH/configs/dlstreamer/models/2022:/home/pipeline-server/models -v $RUN_PATH/configs/dlstreamer/framework-pipelines:/home/pipeline-server/framework-pipelines -w /home/pipeline-server -e BARCODE_RECLASSIFY_INTERVAL=$BARCODE_INTERVAL -e OCR_RECLASSIFY_INTERVAL=$OCR_INTERVAL -e OCR_DEVICE=$OCR_DEVICE -e LOG_LEVEL=$LOG_LEVEL -e GST_DEBUG=$GST_DEBUG -e decode_type="$decode_type" -e pre_process="$pre_process" -e LOW_POWER="$LOW_POWER" -e cid_count=$cid_count -e inputsrc="$inputsrc" $RUN_MODE $stream_density_params -e CPU_ONLY="$CPU_ONLY" -e AUTO_SCALE_FLEX_140="$AUTO_SCALE_FLEX_140" $TAG bash -c "bash $bash_cmd"
