787958616fbe0bf3c911381e02abc496f8dc4bba
Valina Li <valina.li@intel.com>
2023-08-29 16:32:54 -0700

    feat: download all models from list and convert them to IR
    Closes: #242
    Signed-off-by: Valina Li <valina.li@intel.com>

---
+++ download_models/downloadModelsFromList.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 Intel Corporation.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+echo "start download from list models.lst"
+omz_downloader --list models.lst
+omz_converter --list models.lst -o /output/
