01015911bf3be3640c32387e4efa427b3804a43e
Valina Li <valina.li@intel.com>
2023-09-08 11:46:00 -0700

    feat: add at,label,output resolution,threshold env and apply
    Signed-off-by: Valina Li <valina.li@intel.com>

--- configs/opencv-ovms/scripts/run_object_detection.sh
+++ configs/opencv-ovms/scripts/run_object_detection.sh
@@ -27,6 +27,6 @@ docker run --network host --env-file <(env) --privileged $rmDocker \
 -v $RUN_PATH/results:/tmp/results \
 python-demo:dev \
 python3 object_detection/python/object_detection_demo.py -m localhost:"$GRPC_PORT"/models/"$DETECTION_MODEL_NAME" \
---label object_detection/python/labels/person_vehicle_bike_detection_2000.txt -i $inputsrc \
---adapter ovms -t 0.50 -at ssd --output_resolution 1280x720 $mqttArgs \
+--label object_detection/python/labels/"$DETECTION_LABEL_FILE" -i $inputsrc \
+--adapter ovms -t "$DETECTION_THRESHOLD" -at "$DETECTION_ARCHITECTURE_TYPE" --output_resolution "$DETECTION_OUTPUT_RESOLUTION" $mqttArgs \
 2>&1  | tee >/tmp/results/r$cid_count.jsonl >(stdbuf -oL sed -n -e 's/^.*fps: //p' | stdbuf -oL cut -d , -f 1 > /tmp/results/pipeline$cid_count.log)
