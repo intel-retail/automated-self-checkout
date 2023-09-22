ce499b58512cc919998240a3be5bd04a7f58b8b9
Elizabeth J Lee <elizabeth.j.lee@intel.com>
2023-09-21 17:00:41 -0700

    feat: Download public models like the efficientnet classification model
    Signed-off-by: Elizabeth J Lee <elizabeth.j.lee@intel.com>

--- download_models/downloadOVMSModels.sh
+++ download_models/downloadOVMSModels.sh
@@ -93,7 +93,18 @@ downloadOMZmodel(){
 
     (
         # create folder 1 under each precision FP directory to hold the .bin and .xml files
-        cd "$modelDir"/intel || { echo "Error: folder \"intel\" was not created by converter."; exit 1; }
+        omzModelDir=""
+        if [ -d "$modelDir/intel" ]; then
+            omzModelDir="$modelDir/intel"
+        elif [ -d "$modelDir/public" ]; then
+            omzModelDir="$modelDir/public"
+        else
+            echo "Error: folder \"$modelDir/intel\" or \"$modelDir/public\" was not created by converter."
+            exit 1
+        fi
+
+        cd "$omzModelDir" || { echo "Error: could not cd to folder \"$omzModelDir\"." ; exit 1; }
+
         for eachModel in */ ; do
             echo "$eachModel"
             (
@@ -106,10 +117,11 @@ downloadOMZmodel(){
                 done
             )
         done
-    )
 
-    mv "$modelDir"/intel/* "$modelDir"/
-    rm -r "$modelDir"/intel
+        echo "Moving \"$omzModelDir\" to \"$modelDir\""
+        mv "$omzModelDir"/* "$modelDir"/
+        rm -r "$omzModelDir"
+    )
 }
 
 bitModelDirName="BiT_M_R50x1_10C_50e_IR"
