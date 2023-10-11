d62d84fc7beb9dc204bb62798b2cbdc383063688
Valina Li <valina.li@intel.com>
2023-10-11 09:19:40 -0700

    Squashed commit of the following:
    feat: Refactor inputsrc and remove INPUTSRC_TYPE from ovms
    
    Moving gst profile needed env variable manipulations to backend run_gst.sh
    
    Closes: #291
    Signed-off-by: Valina Li <valina.li@intel.com>

--- configs/opencv-ovms/scripts/run_gst.sh
+++ configs/opencv-ovms/scripts/run_gst.sh
@@ -10,8 +10,11 @@ cameras="${cameras:=}"
 stream_density_mount="${stream_density_mount:=}"
 stream_density_params="${stream_density_params:=}"
 cl_cache_dir="${cl_cache_dir:=$HOME/.cl-cache}"
-decode_type="${decode_type:=}"
-pre_process="${pre_process:=}"
+
+COLOR_WIDTH="${COLOR_WIDTH:=1920}"
+COLOR_HEIGHT="${COLOR_HEIGHT:=1810}"
+COLOR_FRAMERATE="${COLOR_FRAMERATE:=15}"
+OCR_SPECIFIED="${OCR_SPECIFIED:=5}"
 
 echo "Run gst pipeline profile"
 rmDocker=--rm
@@ -27,9 +30,38 @@ pipeline="yolov5s.sh"
 
 bash_cmd="/home/pipeline-server/framework-pipelines/yolov5_pipeline/$pipeline"
 
-if grep -q "rtsp" <<< "$inputsrc"; then
+inputsrc=$INPUTSRC
+PRE_PROCESS=""
+if grep -q "rtsp" <<< "$INPUTSRC"; then
 	# rtsp
-	inputsrc=$inputsrc" ! rtph264depay "
+	inputsrc=$INPUTSRC" ! rtph264depay "
+	DECODE="decodebin force-sw-decoders=1"
+	PRE_PROCESS="pre-process-backend=vaapi-surface-sharing -e pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1"
+elif grep -q "file" <<< "$INPUTSRC"; then
+	arrfilesrc=(${INPUTSRC//:/ })
+	# use vids since container maps a volume to this location based on sample-media folder
+	# TODO: need to pass demux/codec info
+	inputsrc="filesrc location=vids/"${arrfilesrc[1]}" ! qtdemux ! h264parse "
+	DECODE="decodebin force-sw-decoders=1"
+	PRE_PROCESS="pre-process-backend=vaapi-surface-sharing -e pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1"
+elif grep -q "video" <<< "$INPUTSRC"; then
+	inputsrc="v4l2src device="$INPUTSRC
+	DECODE="decodebin ! videoconvert ! video/x-raw,format=BGR"
+else
+	# rs-serial realsenssrc
+	# TODO need to pass depthalign info
+	inputsrc="realsensesrc cam-serial-number="$INPUTSRC" stream-type=0 align=0 imu_on=false"
+    # add realsense color related properties if any
+	if [ "$COLOR_WIDTH" != 0 ]; then
+		inputsrc=$inputsrc" color-width="$COLOR_WIDTH
+	fi
+	if [ "$COLOR_HEIGHT" != 0 ]; then
+		inputsrc=$inputsrc" color-height="$COLOR_HEIGHT
+	fi
+	if [ "$COLOR_FRAMERATE" != 0 ]; then
+		inputsrc=$inputsrc" color-framerate="$COLOR_FRAMERATE
+	fi
+	DECODE="decodebin ! videoconvert ! video/x-raw,format=BGR"
 fi
 
 CONTAINER_NAME=gst"$cid_count"
@@ -39,7 +71,7 @@ CONTAINER_NAME=gst"$cid_count"
 docker run --network host $cameras $TARGET_USB_DEVICE $TARGET_GPU_DEVICE --user root --ipc=host \
 --name "$CONTAINER_NAME" \
 -e CONTAINER_NAME="$CONTAINER_NAME" \
--e RENDER_MODE="$RENDER_MODE" $stream_density_mount -e INPUTSRC_TYPE="$INPUTSRC_TYPE" -e DISPLAY="$DISPLAY" \
+-e RENDER_MODE="$RENDER_MODE" $stream_density_mount -e DISPLAY="$DISPLAY" \
 -e cl_cache_dir=/home/pipeline-server/.cl-cache -e RESULT_DIR="/tmp/result" \
 -v "$cl_cache_dir":/home/pipeline-server/.cl-cache -v /tmp/.X11-unix:/tmp/.X11-unix -v "$RUN_PATH"/sample-media/:/home/pipeline-server/vids \
 -v "$RUN_PATH"/configs/dlstreamer/pipelines:/home/pipeline-server/pipelines -v "$RUN_PATH"/configs/dlstreamer/extensions:/home/pipeline-server/extensions \
@@ -47,6 +79,6 @@ docker run --network host $cameras $TARGET_USB_DEVICE $TARGET_GPU_DEVICE --user 
 -v "$RUN_PATH"/configs/dlstreamer/framework-pipelines:/home/pipeline-server/framework-pipelines \
 -w /home/pipeline-server \
 -e BARCODE_RECLASSIFY_INTERVAL="$BARCODE_INTERVAL" -e OCR_RECLASSIFY_INTERVAL="$OCR_INTERVAL" -e OCR_DEVICE="$OCR_DEVICE" -e LOG_LEVEL="$LOG_LEVEL" \
--e GST_DEBUG="$GST_DEBUG" -e decode_type="$decode_type" -e pre_process="$pre_process" -e LOW_POWER="$LOW_POWER" -e cid_count="$cid_count" \
+-e GST_DEBUG="$GST_DEBUG" -e DECODE="$DECODE" -e pre_process="$pre_process" -e LOW_POWER="$LOW_POWER" -e cid_count="$cid_count" \
 -e inputsrc="$inputsrc" $RUN_MODE $stream_density_params -e CPU_ONLY="$CPU_ONLY" -e AUTO_SCALE_FLEX_140="$AUTO_SCALE_FLEX_140" \
 "$TAG" bash -c "bash $bash_cmd"
--- run-ovms.sh
+++ run-ovms.sh
@@ -40,53 +40,16 @@ fi
 cid_count=`ps aux | grep profile-launcher | grep -v grep | wc -l`
 
 #echo "barcode_disabled: $BARCODE_DISABLED, barcode_interval: $BARCODE_INTERVAL, ocr_interval: $OCR_INTERVAL, ocr_device: $OCR_DEVICE, ocr_disabled=$OCR_DISABLED, class_disabled=$CLASSIFICATION_DIABLED"
-pre_process=""
-if grep -q "rtsp" <<< "$INPUTSRC"; then
-	# rtsp
-	# todo pass depay info
-	inputsrc=$INPUTSRC
-	INPUTSRC_TYPE="RTSP"
-	pre_process="pre-process-backend=vaapi-surface-sharing -e pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1"
-
-
-elif grep -q "file" <<< "$INPUTSRC"; then
-	# filesrc	
-	arrfilesrc=(${INPUTSRC//:/ })
-	# use vids since container maps a volume to this location based on sample-media folder
-	# TODO: need to pass demux/codec info
-	inputsrc="filesrc location=vids/"${arrfilesrc[1]}" ! qtdemux ! h264parse "
-	INPUTSRC_TYPE="FILE"
-	decode_type="vaapidecodebin"
-	pre_process="pre-process-backend=vaapi-surface-sharing -e pre-process-config=VAAPI_FAST_SCALE_LOAD_FACTOR=1"
-
+re='^[0-9]+$'
+if [[ "$INPUTSRC" != "rtsp*" && "$INPUTSRC" != "file*" && "$INPUTSRC" != *video* && "$INPUTSRC" =~ $re ]]; then
+	echo "assume realsense device..."
+	cameras=`ls /dev/vid* | while read line; do echo "--device=$line"; done`
+	TARGET_GPU_DEVICE=$TARGET_GPU_DEVICE" "$cameras
 elif grep -q "video" <<< "$INPUTSRC"; then
+	echo "assume video device..."
 	# v4l2src /dev/video*
 	# TODO need to pass stream info
-	inputsrc=$INPUTSRC
-	INPUTSRC_TYPE="USB"
 	TARGET_USB_DEVICE="--device=$INPUTSRC"
-	decode_type="videoconvert ! video/x-raw,format=BGR"
-	pre_process=""
-
-else
-	# rs-serial realsenssrc
-	# TODO need to pass depthalign info
-	inputsrc="realsensesrc cam-serial-number="$INPUTSRC" stream-type=0 align=0 imu_on=false"
-    # add realsense color related properties if any
-	if [ "$COLOR_WIDTH" != 0 ]; then
-		inputsrc=$inputsrc" color-width="$COLOR_WIDTH
-	fi
-	if [ "$COLOR_HEIGHT" != 0 ]; then
-		inputsrc=$inputsrc" color-height="$COLOR_HEIGHT
-	fi
-	if [ "$COLOR_FRAMERATE" != 0 ]; then
-		inputsrc=$inputsrc" color-framerate="$COLOR_FRAMERATE
-	fi
-	INPUTSRC_TYPE="REALSENSE"
-	decode_type="decodebin ! videoconvert ! video/x-raw,format=BGR"
-	pre_process=""
-	cameras=`ls /dev/vid* | while read line; do echo "--device=$line"; done`
-	TARGET_GPU_DEVICE=$TARGET_GPU_DEVICE" "$cameras	
 fi
 
 #pipeline script is configured from configuration.yaml in opencv-ovms/cmd_client/res folder
@@ -136,7 +99,6 @@ TARGET_USB_DEVICE="$TARGET_USB_DEVICE" \
 TARGET_GPU_DEVICE="$TARGET_GPU_DEVICE" \
 MQTT="$MQTT" \
 RENDER_MODE=$RENDER_MODE \
-INPUTSRC_TYPE=$INPUTSRC_TYPE \
 DISPLAY=$DISPLAY \
 cl_cache_dir=$cl_cache_dir \
 RUN_PATH=`pwd` \
@@ -150,7 +112,7 @@ pre_process="$pre_process" \
 LOW_POWER="$LOW_POWER" \
 cid_count=$cid_count \
 STREAM_DENSITY_MODE=$STREAM_DENSITY_MODE \
-inputsrc="$inputsrc" \
+INPUTSRC="$INPUTSRC" \
 STREAM_DENSITY_FPS=$STREAM_DENSITY_FPS \
 STREAM_DENSITY_INCREMENTS=$STREAM_DENSITY_INCREMENTS \
 COMPLETE_INIT_DURATION=$COMPLETE_INIT_DURATION \
