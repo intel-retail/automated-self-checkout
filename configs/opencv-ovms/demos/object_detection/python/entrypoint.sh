35a44f932e79c1cee8842bcc3ad9b69b59523b03
Neethu Elizabeth Simon <neethu.elizabeth.simon@intel.com>
2023-11-06 14:29:14 -0700

    feat: docker-compose changes for ovms client
    Signed-off-by: Neethu Elizabeth Simon <neethu.elizabeth.simon@intel.com>

--- configs/opencv-ovms/demos/object_detection/python/entrypoint.sh
+++ configs/opencv-ovms/demos/object_detection/python/entrypoint.sh
@@ -12,6 +12,10 @@ then
 	mqttArgs="--mqtt ${MQTT}"
 fi
 
+echo "Waiting for OVMS server to be ready...."
+sleep 10
+echo "Done Waiting...."
+
 python3 object_detection/python/object_detection_demo.py -m localhost:"$GRPC_PORT"/models/"$DETECTION_MODEL_NAME" \
 --label object_detection/python/labels/"$DETECTION_LABEL_FILE" -i "$INPUTSRC" \
 --adapter ovms -t "$DETECTION_THRESHOLD" -at "$DETECTION_ARCHITECTURE_TYPE" --output_resolution "$DETECTION_OUTPUT_RESOLUTION" \
