30cef66f50e0cf9ca331c9bc5a28647420f9af5c
Valina Li <valina.li@intel.com>
2023-11-14 08:24:22 -0800

    support input window width and height
    closes: #313
    Signed-off-by: Valina Li <valina.li@intel.com>

--- configs/opencv-ovms/gst_capi/pipelines/face_detection/main.cpp
+++ configs/opencv-ovms/gst_capi/pipelines/face_detection/main.cpp
@@ -495,16 +495,8 @@ std::string getVideoPipelineText(std::string mediaPath, ObjectDetectionInterface
         modelFrameShape = textDet->getModelInputShape();
     }
 
-    int frame_width = modelFrameShape[1];
-    int frame_height = modelFrameShape[2];
-
-    // remove the limitation on render mode only for now
-    // TODO: open it later when supporting custom size is ready
-    // if (_render)
-    {
-        frame_width = _window_width;
-        frame_height = _window_height;
-    }
+    int frame_width = _window_width;
+    int frame_height = _window_height;
 
     return _mediaService->getVideoDecodedPreProcessedPipeline(
         mediaPath,
@@ -720,26 +712,17 @@ void run_stream(std::string mediaPath, GstElement* pipeline, GstElement* appsink
 
         cv::Mat img(_video_input_height, _video_input_width, CV_8UC3, (void *) m.data);
 
-        // remove the limitation on render mode only for now
-        // TODO: open it later when supporting custom size is ready
-        // When rendering is enabled then the input frame is resized to window size and not the needed model input size
-        // if (_render) {
-            if (dynamic_cast<const FaceDetection0005*>(objDet) != nullptr)
-	        {
-                resize(img, analytics_frame, cv::Size(inputShape[2], inputShape[3]), 0, 0, cv::INTER_LINEAR);
-		        hwc_to_chw(analytics_frame, analytics_frame);
-	        }
-            else
-	        {
-                printf("ERROR: Unknown model type\n");
-		        return;
-	        }
-	        analytics_frame.convertTo(floatImage, CV_32F);
-        // }
-        // else {
-        //     hwc_to_chw(img, analytics_frame);
-        //     analytics_frame.convertTo(floatImage, CV_32F);
-        // }
+        if (dynamic_cast<const FaceDetection0005*>(objDet) != nullptr)
+        {
+            resize(img, analytics_frame, cv::Size(inputShape[2], inputShape[3]), 0, 0, cv::INTER_LINEAR);
+            hwc_to_chw(analytics_frame, analytics_frame);
+        }
+        else
+        {
+            printf("ERROR: Unknown model type\n");
+            return;
+        }
+        analytics_frame.convertTo(floatImage, CV_32F);
 
         const int DATA_SIZE = floatImage.step[0] * floatImage.rows;
 
@@ -943,7 +926,7 @@ int main(int argc, char** argv) {
     }
 
     if (!stringIsInteger(argv[2]) || !stringIsInteger(argv[3]) || !stringIsInteger(argv[4])
-        || !stringIsInteger(argv[5])) {
+        || !stringIsInteger(argv[5]) || !stringIsInteger(argv[6]) || !stringIsInteger(argv[7])) {
         print_usage(argv[0]);
         return 1;
     } else {
@@ -951,7 +934,11 @@ int main(int argc, char** argv) {
         _use_onevpl = std::stoi(argv[2]);
         _render = std::stoi(argv[3]);
         _renderPortrait = std::stoi(argv[4]);
-        _videoType = (MediaPipelineServiceInterface::VIDEO_TYPE) std::stoi(argv[5]);        
+        _videoType = (MediaPipelineServiceInterface::VIDEO_TYPE) std::stoi(argv[5]);
+        _window_width = std::stoi(argv[6]);
+        _window_height = std::stoi(argv[7]);
+        std::cout << "_window_width: " << _window_width << std::endl;
+        std::cout << "_window_height: " << _window_height << std::endl;
 
         if (_renderPortrait) {
             int tmp = _window_width;
