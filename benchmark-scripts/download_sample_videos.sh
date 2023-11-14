873b98755b254f651f257395e62c85402c73ce4d
Valina Li <valina.li@intel.com>
2023-11-14 08:43:44 -0800

    update video
    Signed-off-by: Valina Li <valina.li@intel.com>

--- benchmark-scripts/download_sample_videos.sh
+++ benchmark-scripts/download_sample_videos.sh
@@ -9,4 +9,4 @@
 ./format_avc_mp4.sh coca-cola-4465029.mp4 https://www.pexels.com/video/4465029/download/ "$1" "$2" "$3"
 ./format_avc_mp4.sh vehicle-bike.mp4 https://www.pexels.com/video/853908/download/ "$1" "$2" "$3"
 #./format_avc_mp4.sh grocery-items-on-the-kitchen-shelf-4983686.mp4 https://www.pexels.com/video/4983686/download/ $1 $2 $3
-./format_avc_mp4.sh face-demographics-walking.mp4 https://github.com/intel-iot-devkit/sample-videos/blob/master/ "$1" "$2" "$3"
+./format_avc_mp4.sh video_of_people_walking_855564.mp4 https://www.pexels.com/video/855564/download/ "$1" "$2" "$3"
