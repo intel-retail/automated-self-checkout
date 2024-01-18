040c5137e7078b812258cbc2f78ae84030d8443c
Jim Wang <yutsung.jim.wang@intel.com>
2024-01-18 11:05:50 -0700

    feat: add more smoke tests for ARC GPU cases
    CLOSES: #464
    
    Signed-off-by: Jim Wang <yutsung.jim.wang@intel.com>

--- run_smoke_test.sh
+++ run_smoke_test.sh
@@ -316,3 +316,44 @@ verifyStatusCode capi_yolov5_ensemble_gpu $status_code $input_src
 waitForLogFile
 verifyNonEmptyPipelineLog capi_yolov5_ensemble_gpu $input_src
 teardown
+
+source benchmark-scripts/get-gpu-info.sh
+if [ "$HAS_ARC" != 1 ]
+then
+    echo "No ARC GPU: skipping tests on GPU.1"
+    exit 0
+fi
+
+echo "found ARC GPU. run tests on GPU.1..."
+# gst
+echo "Running gst profile on GPU.1 for object detection only..."
+gst_rtsp_input_src="rtsp://127.0.0.1:8554/camera_1"
+PIPELINE_PROFILE="gst" DEVICE="GPU.1" RENDER_MODE=0 sudo -E ./run.sh --platform dgpu.1 --inputsrc "$gst_rtsp_input_src"
+status_code=$?
+verifyStatusCode gst_ARC_gpu_with_detection_only $status_code $gst_rtsp_input_src
+# allowing some time to process
+waitForLogFile
+verifyNonEmptyPipelineLog gst_ARC_gpu_with_detection_only $gst_rtsp_input_src
+teardown
+
+# object_detection
+echo "Running object_detection profile on GPU.1..."
+od_input_src="rtsp://127.0.0.1:8554/camera_1"
+PIPELINE_PROFILE="object_detection" DEVICE="GPU.1" RENDER_MODE=0 sudo -E ./run.sh --platform dgpu.1 --inputsrc "$od_input_src"
+status_code=$?
+verifyStatusCode object_detection_ARC_gpu $status_code $od_input_src
+# allowing some time to process
+waitForLogFile
+verifyNonEmptyPipelineLog object_detection_ARC_gpu $od_input_src
+teardown
+
+# capi_yolov5 ensemble
+echo "Running capi_yolov5_ensemble profile on GPU.1..."
+input_src="rtsp://127.0.0.1:8554/camera_0"
+PIPELINE_PROFILE="capi_yolov5_ensemble" DEVICE="GPU.1" RENDER_MODE=0 sudo -E ./run.sh --platform dgpu.1 --inputsrc "$input_src"
+status_code=$?
+verifyStatusCode capi_yolov5_ensemble_ARC_gpu $status_code $input_src
+# allowing some time to process
+waitForLogFile
+verifyNonEmptyPipelineLog capi_yolov5_ensemble_ARC_gpu $input_src
+teardown
