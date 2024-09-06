3ec56b7e27c98dca5d5f7ded24370e3b6e54abc7
Brian McGinn <brian.mcginn@intel.com>
2024-09-05 21:38:57 -0700

    fix: remove unused servers from status script
    Signed-off-by: Brian McGinn <brian.mcginn@intel.com>

--- src/pipeline-server/status.sh
+++ src/pipeline-server/status.sh
@@ -10,9 +10,5 @@ do
     echo "--------------------- Pipeline Status ---------------------" >> status_results.txt
     echo "----------------8080----------------" >> status_results.txt
     curl --location 'localhost:8080/pipelines/status' >> status_results.txt
-    echo "----------------8081----------------" >> status_results.txt
-    curl --location 'localhost:8071/pipelines/status' >> status_results.txt
-    echo "----------------8082----------------" >> status_results.txt
-    curl --location 'localhost:8072/pipelines/status' >> status_results.txt
     sleep 15
 done
