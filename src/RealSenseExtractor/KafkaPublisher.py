1241a813c4c5e4bebae19a5b6a314c5d538caa56
hemanth-byte <hemanthreddy237@gmail.com>
2025-01-27 19:10:53 -0700

    adding kafka publishing functionality

--- src/RealSenseExtractor/KafkaPublisher.py
+++ src/RealSenseExtractor/KafkaPublisher.py
@@ -1,5 +1,14 @@
 from Publisher import Publisher
+from KafkaCore import produceMessage
 
 class KafkaPublisher(Publisher):
     def push(self, height, width, depth, timestamp):
-        pass
+        message = {}
+        message['height'] = height
+        message['width'] = width
+        message['timestamp'] = timestamp
+        message['depth'] = depth
+        produceMessage(message)
+        
+
+        
